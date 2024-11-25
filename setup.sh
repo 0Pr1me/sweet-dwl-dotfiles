cce86270c4f13291fc578c18a05b42691500274b
MikolajFrycz2115 <mikolajfryczv2@gmail.com>
2024-11-26 00:12:19 +0100

    added blueman

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ pacman -Syu --noconfirm
 
 # Install packages via pacman (official repos)
 echo "Installing official packages via pacman..."
-pacman -S --noconfirm wayland wayland-protocols copyq waybar swaylock thunar dolphin gammastep firefox chromium rofi-wayland lsd fastfetch alacritty
+pacman -S --noconfirm wayland wayland-protocols copyq waybar swaylock thunar dolphin gammastep firefox chromium rofi-wayland lsd fastfetch alacritty blueman
 
 # Install AUR packages via yay
 echo "Installing AUR packages via yay..."
